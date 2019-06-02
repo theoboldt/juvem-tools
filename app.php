834fa8b122d82238b9342e9d7c7e8e13ac003f72
theoboldt <erik@theoboldt.eu>
2019-06-02 23:09:33 +0200

    Using stdout

--- app.php
+++ app.php
@@ -13,13 +13,13 @@ set_time_limit(60*60*60);
 $output = null;
 
 if (array_key_exists('apt-update', $_GET)) {
-    $output = `sudo /usr/bin/apt -qq update`;
+    $output = `sudo /usr/bin/apt -qq update 2>&1`;
 }
 if (array_key_exists('apt-dist-upgrade', $_GET)) {
-    $output = `sudo /usr/bin/apt -dist-upgrade && /usr/bin/apt -y autoremove && /usr/bin/apt -y autoclean`;
+    $output = `sudo /usr/bin/apt -dist-upgrade 2>&1 && /usr/bin/apt -y autoremove 2>&1 && /usr/bin/apt -y autoclean 2>&1`;
 }
 if (array_key_exists('tools', $_GET)) {
-    $output = `cd /var/www/juvem-tools && /usr/bin/git pull`;
+    $output = `cd /var/www/juvem-tools && /usr/bin/git pull 2>&1`;
 }
 
 if ($output !== null) {
