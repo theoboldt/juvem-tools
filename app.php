66fdcda8a25ea2f7a0e6a42a9f1829c151f8a065
theoboldt <erik@theoboldt.eu>
2019-06-02 23:26:55 +0200

    Provide cache clear

--- app.php
+++ app.php
@@ -21,6 +21,9 @@ if (array_key_exists('apt-dist-upgrade', $_GET)) {
 if (array_key_exists('tools', $_GET)) {
     $output = `cd /var/www/juvem-tools && /usr/bin/git pull 2>&1`;
 }
+if (array_key_exists('cache', $_GET)) {
+    $output = `sudo /bin/rm -rf /var/www/juvem/app/cache/*`;
+}
 
 if ($output !== null) {
     echo '<pre>'.$output.'</pre>';
@@ -31,12 +34,18 @@ if ($output !== null) {
 <h1>Tools</h1>
 <ul>
     <li>
+        <a href="/app.php?cache=1">Cache leeren</a>
+    </li>
+</ul>
+<h2>Server</h2>
+<ul>
+    <li>
         <a href="/app.php?apt-update=1">Paketliste aktualisieren</a>
     </li>
     <li>
         <a href="/app.php?apt-dist-upgrade=1">Pakete aktualisieren</a>
     </li>
     <li>
-        <a href="/app.php?tools=1">Tools aktualisieren</a>
+        <a href="/self.php">Tools aktualisieren</a>
     </li>
 </ul>
